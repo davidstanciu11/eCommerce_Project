c5c79e208e10972f72ba1fc74423b2e30b6fc5e9
davidstanciu11 <stanciudavid169@gmail.com>
2023-03-09 13:29:18 +0200

    carrousel ready

--- src/components/BootstrapCarousel.jsx
+++ src/components/BootstrapCarousel.jsx
@@ -1,33 +1,41 @@
 import Carousel from "react-bootstrap/Carousel";
 import cabinet from "../../assets/cabinet.jpg";
-import chair from "../../assets/chair.jpg";
+import chair from "../../assets/chair.webp";
 import sofa from "../../assets/sofa.jpg";
 import "./BootstrapCarousel.css";
+import { Button } from "bootstrap";
 
 function DarkVariant() {
 	return (
-		<Carousel variant='dark'>
-			<Carousel.Item><div id="imges"><img className='show_pics' src={sofa} alt='First slide' /></div>
-				
+		<Carousel variant='dark' className='carousel'>
+			<Carousel.Item>
+				<img className='show_pics' src={sofa} alt='First slide' />
 				<Carousel.Caption>
-					<h5>First slide label</h5>
-					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					<h5 className='title_product'>Brown Leather Sofa</h5>
+					<p className='description_product'>Newest Collection &#169; 2023</p>
+					<button type='button' class='btn btn-outline-light' id='button'>
+						Discover
+					</button>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img className='show_pics' src={cabinet} alt='Second slide' />
-				<Carousel.Caption>
-					<h5>Second slide label</h5>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<Carousel.Caption className='caption_1'>
+					<h5 className='title_product1'>Kitchen Set</h5>
+					<p className='description_prodcut1'>Newest Collection &#169; 2023</p>
+					<button type='button' class='btn btn-outline-dark' id='button_1'>
+						Discover
+					</button>
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
 				<img className='show_pics' src={chair} alt='Third slide' />
 				<Carousel.Caption>
-					<h5>Third slide label</h5>
-					<p>
-						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-					</p>
+					<h5 id="title_3">Mugler Chair</h5>
+					<p id="caption_3">Newest Collection &#169; 2023</p>
+					<button type='button' class='btn btn-outline-dark' id='button_2'>
+						Discover
+					</button>
 				</Carousel.Caption>
 			</Carousel.Item>
 		</Carousel>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,7 @@ function Home() {
 	return (
 		<>
 			<Navigation />
-            <div><DarkVariant/></div>
-            
+            <DarkVariant/>
 		</>
 	);
 }
